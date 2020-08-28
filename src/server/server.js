cb253248a878e6b91a316f501fececb5c01d5af9
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-08-28 12:55:48 -0500

    still not working

--- src/server/server.js
+++ src/server/server.js
@@ -1,27 +1,33 @@
 import grpc from 'grpc';
 import protos from '@jocampo/simple-grpc-chat-proto/proto-objects.js';
 
+const chatServiceProto = grpc.loadPackageDefinition(protos).services;
+console.log(chatServiceProto)
+
 const server = new grpc.Server();
 const SERVER_ADDRESS = "0.0.0.0:5001";
 
 const users = [];
 
-const chat = (call) => {
+const chat = (call, callback) => {
     users.push(call);
     notifyChat({ user: "Server", text: "new user joined ..." });
 };
 
-const sendMessage = (call) => {
+const sendMessage = (call, callback) => {
     notifyChat(call.request);
 };
 
-const notifyChat = (message) => {
+const notifyChat = (message, callback) => {
     users.forEach(user => {
         user.write(message);
     });
 }
 
-server.addService(protos.services.ChatService, { join_chat: chat, send_message: sendMessage });
+server.addService(protos.services.ChatService, {
+    join_chat: chat,
+    send_message: sendMessage
+});
 
 server.bind(SERVER_ADDRESS, grpc.ServerCredentials.createInsecure());
 
